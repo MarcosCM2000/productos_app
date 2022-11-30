173408fa341750148dc40f95820ae72e5ea1ec7b
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-11-29 18:32:59 -0600

    login form edited

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './../widgets/widgets.dart';
+import './../ui/input_decorations.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -20,7 +21,15 @@ class LoginScreen extends StatelessWidget {
               const SizedBox(height: 30),
               const _LoginForm()
             ]),
-          )
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text('Crear cuenta nueva',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          const SizedBox(
+            height: 50,
+          ),
         ]),
       )),
     );
@@ -40,19 +49,41 @@ class _LoginForm extends StatelessWidget {
           TextFormField(
             autocorrect: false,
             keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.deepPurple)),
-                focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.deepPurple, width: 2)),
-                hintText: 'Email',
+            decoration: InputDecorations.authInputDecoration(
+                hintText: 'john.doe@gmail.com',
                 labelText: 'Correo electronico',
-                labelStyle: TextStyle(color: Colors.grey),
-                prefixIcon: Icon(
-                  Icons.alternate_email_sharp,
-                  color: Colors.deepPurple,
-                )),
-          )
+                prefixIcon: Icons.alternate_email_outlined),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.visiblePassword,
+            decoration: InputDecorations.authInputDecoration(
+                hintText: '*****',
+                labelText: 'Password',
+                prefixIcon: Icons.lock_outline),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          MaterialButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                child: Text(
+                  'Ingresar',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              onPressed: () {
+                //  TODO: Login form
+              })
         ],
       )),
     );
