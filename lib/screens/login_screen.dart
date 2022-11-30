ea50fd2c26ba845a7fced7cb435adb2ad368af49
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-11-29 18:02:04 -0600

    beggining of login form

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -18,10 +18,42 @@ class LoginScreen extends StatelessWidget {
               const SizedBox(height: 10),
               Text('LOGIN', style: Theme.of(context).textTheme.headline4),
               const SizedBox(height: 30),
-              const Text('FORM')
+              const _LoginForm()
             ]),
           )
         ]),
+      )),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+          //  TODO: mantener la referencia al key
+          child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepPurple)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepPurple, width: 2)),
+                hintText: 'Email',
+                labelText: 'Correo electronico',
+                labelStyle: TextStyle(color: Colors.grey),
+                prefixIcon: Icon(
+                  Icons.alternate_email_sharp,
+                  color: Colors.deepPurple,
+                )),
+          )
+        ],
       )),
     );
   }
